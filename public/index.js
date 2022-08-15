dc2fb71603078ff5e2f5f70bd09913e5f9cb0242
ibnyahyah <yahyahridwan665@gmail.com>
2022-08-15 18:33:42 +0100

    adding faqs, policy,terms and conditions, refund policy page

--- public/index.js
+++ public/index.js
@@ -11,3 +11,37 @@ hamburger.addEventListener('click', () => {
 })
 
 AOS.init();
+
+
+const paymentHandler = document.getElementById("payment");
+const courseHandler = document.getElementById("course");
+const certificateHandler = document.getElementById("certificate");
+const paymentFaqs = document.getElementById("payments-faqs");
+const courseFaqs = document.getElementById("course-faqs");
+const certificateFaqs = document.getElementById("certificate-faqs");
+
+
+courseHandler.addEventListener("click", () => {
+	courseFaqs.style.display = "block";
+	paymentFaqs.style.display = "none";
+	certificateFaqs.style.display = "none";
+	courseHandler.className = "btn btn-primary";
+	paymentHandler.className = "btn btn-outline-primary";
+	certificateHandler.className = "btn btn-outline-primary";
+});
+paymentHandler.addEventListener("click", () => {
+	paymentFaqs.style.display = "block";
+	courseFaqs.style.display = "none";
+	certificateFaqs.style.display = "none";
+	paymentHandler.className = "btn btn-primary";
+	courseHandler.className = "btn btn-outline-primary";
+	certificateHandler.className = "btn btn-outline-primary";
+});
+certificateHandler.addEventListener("click", () => {
+	certificateFaqs.style.display = "block";
+	paymentFaqs.style.display = "none";
+	courseFaqs.style.display = "none";
+	certificateHandler.className = "btn btn-primary";
+	paymentHandler.className = "btn btn-outline-primary";
+	courseHandler.className = "btn btn-outline-primary";
+});
